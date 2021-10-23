1a8ef39d3c65b94bf63e43422f751c546626c72f
Raisa_Dev <73198136+raisa-design@users.noreply.github.com>
2021-10-23 17:58:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,27 +15,63 @@ function changeSocialMediaLinks() {
     } else {
       li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
     }
-
-  
   }
 }
 
 changeSocialMediaLinks()
 
-function getGithubProfilesInfos(){
- const url = `https://api.github.com/users/${linksSocialMedia.github} `
- 
+var speed = 120
+var cont = 0
+
+function typeWriter(content) {
+  userBio.textContent = ''
+  function write() {
+    if (cont < content.length) {
+      userBio.textContent += content.charAt(cont)
+      cont++
+      setTimeout(write, speed)
+    } else {
+      userBio.textContent = ''
+      cont = 0
+      write()
+    }
+  }
+  write()
+}
+
+function getGithubProfilesInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github} `
+
   //alert(url)
 
   fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    userName.textContent= data.name
-    userBio.textContent=data.bio
-    userLink.href=data.html_url
-    userImage.src=data.avatar_url
-    userLogin.textContent=data.login
-  })
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      typeWriter(data.bio)
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+      document.getElementById('userImage').classList.add('animate-image')
+    })
 }
 
 getGithubProfilesInfos()
+
+function changeBackground(dark) {
+  if (dark == true) {
+    main.classList.remove('bg-light')
+    main.classList.add('bg-dark')
+    btnLight.classList.remove('btn-hide')
+    btnLight.classList.add('btn-show')
+    btnDark.classList.remove('btn-show')
+    btnLight.classList.add('btn-hide')
+  } else {
+    main.classList.remove('bg-dark')
+    main.classList.add('bg-light')
+    btnDark.classList.remove('btn-hide')
+    btnDark.classList.add('btn-show')
+    btnLight.classList.remove('btn-show')
+    btnLight.classList.add('btn-hide')
+  }
+}
